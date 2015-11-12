19a9f56f51765d1ea499a59e7295a9fb80073de2
Justin Tormey <jrtormey@gmail.com>
2015-11-12 10:34:02 -0500

    caching module for storing many wallet objects

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 'use strict';
 
-var bc  = require('blockchain-wallet-client')
-  , q   = require('q');
+var q     = require('q')
+  , Cache = require('./cache');
+
+var cache = new Cache();
 
 module.exports = {
   login             : login,
@@ -11,42 +13,45 @@ module.exports = {
 };
 
 function login(guid, options) {
-  var MyWallet = bc.MyWallet
-    , deferred = q.defer();
-  var needs2FA  = deferred.reject.bind(null, 'ERR_2FA')
+  var MyWallet = require('blockchain-wallet-client').MyWallet;
+  var deferred = q.defer()
+    , needs2FA  = deferred.reject.bind(null, 'ERR_2FA')
     , error     = deferred.reject.bind(null, 'ERR_SAVING');
   function success() {
     var resolve = deferred.resolve.bind(null, { guid: guid, success: true })
       , reject  = deferred.reject.bind(null, 'ERR_HISTORY');
-    bc.MyWallet.wallet.getHistory().then(resolve).catch(reject);
+    MyWallet.wallet.getHistory().then(resolve).catch(reject);
+    cache.save(MyWallet.wallet);
   }
   function tryLogin() {
     clearCache();
-    bc = require('blockchain-wallet-client');
-    bc.MyWallet.login(guid, null, options.password, null, success, needs2FA, null, null, error);
+    MyWallet.login(guid, null, options.password, null, success, needs2FA, null, null, error);
     return deferred.promise;
   }
   return tryLogin();
 }
 
 function getBalance(guid, options) {
-  var wallet = bc.MyWallet.wallet;
-  return q({ balance: wallet.finalBalance });
+  return cache.wallet(guid).then(function (wallet) {
+    return { balance: wallet.finalBalance };
+  });
 }
 
 function listAddresses(guid, options) {
-  var wallet = bc.MyWallet.wallet
-    , addresses = wallet.keys.map(addressFactory);
-  return q({ addresses: addresses });
+  return cache.wallet(guid).then(function (wallet) {
+    var addresses = wallet.keys.map(addressFactory);
+    return { addresses: addresses };
+  });
   function addressFactory(a) {
     return {address: a.address, label: a.label, balance: a.balance, total_received: a.totalReceived};
   }
 }
 
 function getAddressBalance(guid, options) {
-  var wallet  = bc.MyWallet.wallet
-    , addr    = wallet.key(options.address);
-  return q({ balance: addr.balance, address: addr.address, total_received: addr.totalReceived });
+  return cache.wallet(guid).then(function (wallet) {
+    var addr = wallet.key(options.address);
+    return { balance: addr.balance, address: addr.address, total_received: addr.totalReceived };
+  });
 }
 
 // Helper functions
---
+++ src/cache.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var q = require('q');
+
+function Cache() {
+  this._wallets = {};
+}
+
+Object.defineProperties(Cache.prototype, {
+  guids: {
+    configurable: false,
+    get: function () { return Object.keys(this._wallets); }
+  },
+  wallet: {
+    configurable: false,
+    value: function (guid) {
+      return this._wallets[guid] != null ?
+        q(this._wallets[guid]) : q.reject('ERR_WALLET_ID');
+    }
+  }
+});
+
+Cache.prototype.save = function (wallet) {
+  this._wallets[wallet.guid] = wallet;
+};
+
+Cache.prototype.remove = function (guid) {
+  delete this._wallets[guid];
+};
+
+module.exports = Cache;
